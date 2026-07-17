0fc3dd8151412957416f7913967db7accf6ca865
Daniel Simonkay <daniel.simonkay@partner.bmw.de>
2026-07-17 09:19:28 +0200

    Initial commit

---
+++ mandelbrot.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/// @brief Configuration object for rendering a single frame
+struct Config
+{
+    double start_real{-2.5}; ///< Range start on the real axis
+    double end_real{1.0};    ///< Range end on the real axis
+    double start_i{-1.0};    ///< Range start on the imaginary axis
+    double end_i{1.0};       ///< Range end on the imaginary axis
+};
+
+int main()
+{
+    /// TODO Add implementation
+
+    return 0;
+}
